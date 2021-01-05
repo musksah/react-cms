0a33c1533a32dd7e41eda10f60dbc0fff6f5fd88
Sebastián Huérfano <xsebastianhrodriguezx@gmail.com>
2021-01-04 21:10:24 -0500

    Cambio de url del api en producción.

--- src/enviroment.js
+++ src/enviroment.js
@@ -1,5 +1,5 @@
 const URL_LOCAL = "http://localhost:8005/api";
-const URL_PRODUCCION = "http://sebastianhr-services.site/api";
+const URL_PRODUCCION = "https://fullipscolombia-services.site/api";
 export default {
-    urlApi: URL_PRODUCCION,
+    urlApi: URL_LOCAL,
 }
